fb6a4b1d9777283efe427cd8cb2fe8ab810462b4
Pawel <52752634+AlliSaw@users.noreply.github.com>
2020-02-19 18:09:56 +0000

    last

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,16 @@
             this.btnsearch = new System.Windows.Forms.Button();
             this.btnamb = new System.Windows.Forms.Button();
             this.txtGps = new System.Windows.Forms.TextBox();
+            this.lblCounty = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.wbHosp = new System.Windows.Forms.WebBrowser();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnsearch
             // 
-            this.btnsearch.Location = new System.Drawing.Point(144, 117);
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch.Location = new System.Drawing.Point(77, 119);
             this.btnsearch.Name = "btnsearch";
             this.btnsearch.Size = new System.Drawing.Size(129, 30);
             this.btnsearch.TabIndex = 0;
@@ -45,7 +50,8 @@
             // 
             // btnamb
             // 
-            this.btnamb.Location = new System.Drawing.Point(144, 206);
+            this.btnamb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnamb.Location = new System.Drawing.Point(77, 200);
             this.btnamb.Name = "btnamb";
             this.btnamb.Size = new System.Drawing.Size(129, 30);
             this.btnamb.TabIndex = 2;
@@ -54,22 +60,69 @@
             // 
             // txtGps
             // 
-            this.txtGps.Location = new System.Drawing.Point(144, 29);
+            this.txtGps.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGps.Location = new System.Drawing.Point(77, 355);
+            this.txtGps.Multiline = true;
             this.txtGps.Name = "txtGps";
-            this.txtGps.Size = new System.Drawing.Size(100, 20);
+            this.txtGps.Size = new System.Drawing.Size(354, 201);
             this.txtGps.TabIndex = 3;
             this.txtGps.TextChanged += new System.EventHandler(this.txtGps_TextChanged);
             // 
+            // lblCounty
+            // 
+            this.lblCounty.AutoSize = true;
+            this.lblCounty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounty.Location = new System.Drawing.Point(73, 43);
+            this.lblCounty.Name = "lblCounty";
+            this.lblCounty.Size = new System.Drawing.Size(126, 20);
+            this.lblCounty.TabIndex = 4;
+            this.lblCounty.Text = "Enter the county";
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLocation.Location = new System.Drawing.Point(279, 43);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(170, 26);
+            this.txtLocation.TabIndex = 5;
+            // 
+            // wbHosp
+            // 
+            this.wbHosp.Location = new System.Drawing.Point(502, 43);
+            this.wbHosp.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbHosp.Name = "wbHosp";
+            this.wbHosp.Size = new System.Drawing.Size(844, 576);
+            this.wbHosp.TabIndex = 6;
+            this.wbHosp.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbHosp_DocumentCompleted);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(77, 276);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(129, 29);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
             // Form1
             // 
+            this.AcceptButton = this.btnsearch;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(1437, 674);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.wbHosp);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.lblCounty);
             this.Controls.Add(this.txtGps);
             this.Controls.Add(this.btnamb);
             this.Controls.Add(this.btnsearch);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "E-Care";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +133,10 @@
         private System.Windows.Forms.Button btnsearch;
         private System.Windows.Forms.Button btnamb;
         private System.Windows.Forms.TextBox txtGps;
+        private System.Windows.Forms.Label lblCounty;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.WebBrowser wbHosp;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 //using System.Device.Location;
+
 
 namespace ECare
 {
@@ -25,12 +27,70 @@ namespace ECare
         
         private void btnsearch_Click(object sender, EventArgs e)
         {
-            Form1 Form2 = new Form1();
-            //Form2.MdiParent = this;
-            Form2.Show();
+            List<string> hospList = new List<string>();
+            List<string> hospLists = new List<string>();
+            List<string> listA = new List<string>();
+            int listLen = 0;
+
+            using (var reader = new StreamReader(@"C:\GitHub\ECare\ECare\db.csv"))
+            {
+                string tableName = "Hospitals";
+                DataSet dataset = new DataSet();
+                dataset.Tables.Add(tableName);
+                dataset.Tables[tableName].Columns.Add("Number");
+                dataset.Tables[tableName].Columns.Add("Name");
+                dataset.Tables[tableName].Columns.Add("Select");
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    if(values[1] == txtLocation.Text)
+                    {
+                        listA.Add(values[0]);
+                    }
+                    listLen = listA.Count;
+
+                }
+
+                
+                
+
+
+                for (int i = 0; i < listLen; i++)
+                {
+
+                    txtGps.AppendText(listA[i]+Environment.NewLine);
+
+                    
+                }
+            }
+
+            StringBuilder location1 = new StringBuilder();
+
+            location1.Append("http://maps.google.ie/maps?q=");
+            location1.Append(txtLocation.Text + " hospital");
+            wbHosp.Navigate(location1.ToString());
+            wbHosp.ScriptErrorsSuppressed = true;
         }
 
         private void txtGps_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wbHosp_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtGps.Text = "";
+            txtLocation.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
